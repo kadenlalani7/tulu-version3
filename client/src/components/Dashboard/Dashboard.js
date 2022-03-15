a83f91649c5a06f968247747b5792137e91f7d62
Chase Brown <Chasebrown7@icloud.com>
2022-03-15 16:52:18 -0700

    css fixes

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -15,28 +15,25 @@ const Dashboard = () => {
 
 
     return(
-        <div className="relative background pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="relative background pt-8 pb-20 px-4 sm:px-6 lg:pt-12 lg:pb-28 lg:px-8">
             
             <div className="absolute inset-0">
                 <div className="h-1/3 sm:h-2/3" />
             </div>
             <div className="relative max-w-7xl mx-auto">
-                <div className="text-center">
-                    <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">tulu</h2>
+                <div className="">
+                    <h1 className="font-medium text-9xl flex justify-center text-white">
+                        tulu.
+                    </h1>
                 </div>
-                <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+                <div className="mt-24 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
 
                     <div className="flex flex-col rounded-[50px] shadow-lg overflow-hidden h-[576px] w-[370px] " onClick={handleExplore}>
                         {/* image */}
-                        <div className="flex-1 bg-teal-900 p-6 flex flex-col justify-between">
+                        <div className="flex-1 bg-[#084C5E] p-6 flex flex-col justify-between">
                             <div className="flex-1">
-                                {/* <p className="text-sm font-medium text-indigo-600">
-                                    <a className="hover:underline">
-                                        category name
-                                    </a>
-                                </p> */}
                                 <a className="block mt-2">
-                                    <p className="text-center text-[48px] font-semibold text-gray-900">explore</p>
+                                    <p className="text-center text-[48px] font-bold text-white underline underline-offset-[10px]">explore</p>
                                 </a>
                             </div>
                         </div>
@@ -46,13 +43,8 @@ const Dashboard = () => {
                         {/* image */}
                         <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                             <div className="flex-1">
-                                {/* <p className="text-sm font-medium text-indigo-600">
-                                    <a className="hover:underline">
-                                        category name
-                                    </a>
-                                </p> */}
                                 <a className="block mt-2">
-                                    <p className="text-center text-[48px] font-semibold text-gray-900">plan</p>
+                                    <p className="text-center text-[48px] font-bold text-black underline underline-offset-[10px]">plan</p>
                                 </a>
                             </div>
 
@@ -61,18 +53,12 @@ const Dashboard = () => {
                     
                     <div className="flex flex-col rounded-[50px] shadow-lg overflow-hidden h-[576px] w-[370px]" onClick={handleConnect}>
                         {/* image */}
-                        <div className="flex-1 bg-stone-500 p-6 flex flex-col justify-between">
+                        <div className="flex-1 bg-[#7C746D] p-6 flex flex-col justify-between">
                             <div className="flex-1">
-                                {/* <p className="text-sm font-medium text-indigo-600">
-                                    <a className="hover:underline">
-                                        category name
-                                    </a>
-                                </p> */}
                                 <a className="block mt-2">
-                                    <p className="text-center text-[48px] font-semibold text-gray-900">connect</p>
+                                    <p className="text-center text-[48px] font-bold text-white underline underline-offset-[10px]">connect</p>
                                 </a>
                             </div>
-
                         </div>
                     </div>
                 </div>
